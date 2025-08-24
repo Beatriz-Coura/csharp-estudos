a250b4c83e0e1512714cb05618d05507481e6e1a
Beatriz Coura <beatrizfcoura@gmail.com>
2025-08-24 17:11:57 -0300

    feat: desenvolver exercício 02 de lógica em C#

---
+++ 01-logica/exercicio02/Program.cs
@@ -0,0 +1,22 @@
+﻿//Exercício 2: Escreva um programa que leia um número inteiro e diga se ele é par ou ímpar.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Digite um número");
+        int num = int.Parse(Console.ReadLine());
+
+        if (num % 2 == 0)
+        {
+            Console.WriteLine($"O número {num} é par");
+        }
+        else
+        {
+            Console.WriteLine($"O número {num} é impar");
+        }
+    }
+}
