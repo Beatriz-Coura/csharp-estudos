4691f32e76cbd10b62dc2ab07b51a00693c8d924
Beatriz Coura <beatrizfcoura@gmail.com>
2025-08-24 18:19:02 -0300

    feat: desenvolver exercício 04 de lógica em C#

---
+++ 01-logica/exercicio04/Program.cs
@@ -0,0 +1,30 @@
+﻿//Exercício 4: Calcule a média de 3 notas e informe se o aluno está aprovado (média ≥ 7) ou reprovado.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite a primeira nota: ");
+        double nota1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a segunda nota: ");
+        double nota2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a terceira nota: ");
+        double nota3 = double.Parse(Console.ReadLine());
+
+        double media = (nota1 + nota2 + nota3) / 3.0;
+
+        if(media >= 7)
+        {
+            Console.WriteLine($"A média é {media}, então o aluno está aprovado");
+        }
+        else {
+                Console.WriteLine($"A média é {media}, então o aluno está reprovado");
+            }
+
+
+        }
+}
