3e7b62915514106643e063a857968a4eccd57aef
Beatriz Coura <beatrizfcoura@gmail.com>
2025-08-24 19:41:31 -0300

    feat: desenvolver exercício 05 de lógica em C#

---
+++ 01-logica/exercicio05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número: ");
+         int num = int.Parse(Console.ReadLine());
+        int fat = 1;
+        while(num != 1)
+        {
+            fat *= num;
+            num--;
+        }
+
+        Console.WriteLine($"O fatorial é: {fat}");
+        
+    }
+}
