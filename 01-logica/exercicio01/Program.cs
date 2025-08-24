9ca123f487070877887ebffa555302b0bdceb0eb
Beatriz Coura <beatrizfcoura@gmail.com>
2025-08-24 16:49:16 -0300

    feat: desenvolver exercício 01 de lógica em C#

---
+++ 01-logica/exercicio01/Program.cs
@@ -0,0 +1,27 @@
+﻿//Exercício 1: Crie um algoritmo que receba dois números e mostre a soma, subtração, multiplicação e divisão.
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o primeiro número: ");
+        double num1 = double.Parse(Console.ReadLine());
+        Console.Write("Digite o segundo número: ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+        Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+        Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+        if (num2 != 0)
+        {
+            Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+        }
+        else
+        {
+            Console.WriteLine("Não é possível dividir por zero!");
+        }
+
+    }
+}
+
