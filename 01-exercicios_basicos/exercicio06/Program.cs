31704d54738cf449edb102b710bfd07d659207ba
Beatriz Coura <beatrizfcoura@gmail.com>
2025-10-03 09:56:20 -0300

    feat: desenvolver exercício 06 em C#

---
+++ 01-exercicios_basicos/exercicio06/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Escreva seu nome: ");
+        string nome = Console.ReadLine();
+        Console.Write($"Olá, {nome}");
+    }
+}
